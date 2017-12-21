ee5b8f2b786d961b4379dff7edc50fcfcb7e0e21
Bao Tran <baontq@gmail.com>
2017-12-21 15:51:30 -0500

    Update binancews.js

--- binancews.js
+++ binancews.js
@@ -4,7 +4,14 @@ binance.options({
 	'APISECRET':'{process.env.B_SECRET}'
 });
 
+var graphite = require('graphite');
+var client = graphite.createClient('plaintext://localhost:2003/');
+
 binance.websockets.trades(['BNBBTC', 'ETHBTC'], function(trades) {
 	let {e:eventType, E:eventTime, s:symbol, p:price, q:quantity, m:maker, a:tradeId} = trades;
 	console.log(symbol+" trade update. price: "+price+", quantity: "+quantity+", maker: "+maker);
+	var metrics = {"{symbol}.volumn": quantity, "{symbol}.price": price};
+	client.write(metrics, function(err) {
+		  // if err is null, your data was sent to graphite!
+	});
 });
