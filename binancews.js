95d24a0f63ac952e94bc3f9be8143d163363072d
Bao Tran <baotran@boomi.com>
2017-12-21 17:14:35 -0500

    add another ticker to test

--- binancews.js
+++ binancews.js
@@ -7,12 +7,12 @@ binance.options({
 var graphite = require('graphite');
 var client = graphite.createClient('plaintext://localhost:2003/');
 
-binance.websockets.trades(['BNBBTC'], function(trades) {
+binance.websockets.trades(['BNBBTC', 'ETHBTC'], function(trades) {
 	let {e:eventType, E:eventTime, s:symbol, p:price, q:quantity, m:maker, a:tradeId} = trades;
 	//console.log(symbol+" trade update. price: "+price+", quantity: "+quantity+", maker: "+maker);
 
     var metrics = {
-        'BNBBTC' : {
+        [symbol] : {
             'volume' : quantity,
             'price' : price
         }
